d16cf5551c168005fe7472ef77e8f3a36350b272
Leszek Pryszcz <lpryszcz@i7-k4770>
2014-03-13 14:02:03 +0100

    plot_hits.py multi panels

--- vcf2fasta.py
+++ vcf2fasta.py
@@ -9,11 +9,11 @@ l.p.pryszcz@gmail.com
 Girona/Cracow, 6/11/2012
 """
 
-import argparse, os, sys
+import argparse, os, random, sys
 from datetime import datetime
 from Bio      import SeqIO
 
-def load_vcf(vcf, noindels, verbose):
+def load_vcf(vcf, noindels, nohetero, verbose):
     """Return dictionary of variants
     with 0-based, half-open coordinates (BED,python).
     VCF is 1-based (GFF like) ?
@@ -21,18 +21,23 @@ def load_vcf(vcf, noindels, verbose):
     chr2variants = {}
     for l in vcf:
         l = l.strip()
-        if not l or l.startswith("#"):
+        if not l or l.startswith("#") or "PASS" not in l:
             continue
-        contig,pos,id,ref,alt,qual,flt = l.split("\t")[:7]
+        contig, pos, id, ref, alt, qual, flt = l.split("\t")[:7]
         #0-based coordinates
         pos = int(pos)-1
         if noindels and len(ref)!=len(alt):
             continue
+        #skip heterozygous SNPs if defined so
+        if '0/1' in l:
+            #or select random
+            if nohetero or random.randint(0, 1):
+                continue
         #add chr to dict
         if not contig in chr2variants:
             chr2variants[contig]=[]
         #append
-        chr2variants[contig].append( (pos,ref,alt) )
+        chr2variants[contig].append((pos, ref, alt))
     return chr2variants
 
 def variants2sequence( gene,seq,vof,qsorg,verbose ):
@@ -43,7 +48,7 @@ def variants2sequence( gene,seq,vof,qsorg,verbose ):
     #introduce variants from the end, so indels will not mess up coordinates
     snps = indels = 0
     #bigindels = []
-    for opos,ref,alt in sorted( vof,reverse=True ):
+    for opos, ref, alt in sorted(vof, reverse=True):
         #update pos
         pos = opos - qsorg
         #warn about indel
@@ -57,7 +62,8 @@ def variants2sequence( gene,seq,vof,qsorg,verbose ):
         #replace orginal seq
         #print seq
         seq = seq[:pos] + alt + seq[pos+len(ref):]
-        sys.stderr.write( "%s: %s > %s\n" % (pos,ref,alt))
+        if verbose:
+            sys.stderr.write( "%s: %s > %s\n" % (pos,ref,alt))
         #print seq; print 
 
     #report large indels
@@ -65,42 +71,48 @@ def variants2sequence( gene,seq,vof,qsorg,verbose ):
     #    sys.stderr.write( " Big indels in %s: %s\n" % (gene,str(bigindels)) )
     return ( str(seq),snps,indels )
     
-def vcf2fasta( out,vcf,genome,onebased,noindels,verbose ):
+def vcf2fasta(out, vcf, genome, noindels, nohetero, verbose):
     """
     """
     #load vcf file
-    chr2variants = load_vcf(vcf, noindels, verbose)
+    chr2variants = load_vcf(vcf, noindels, nohetero, verbose)
 
     #process all entries
-    for r in SeqIO.parse( genome,"fasta" ):
+    for r in SeqIO.parse(genome, "fasta"):
         q = r.id
         vof = []
         #carefully check if chr1:100-200 or just chr1
-        if len(q.split(":"))==2 and len(q.split(":")[1].split("-"))==2 and q.split(":")[1].split("-")[0].isdigit() and q.split(":")[1].split("-")[1].isdigit():
+        if len(q.split(":"))==2 and len(q.split(":")[1].split("-"))==2 \
+           and q.split(":")[1].split("-")[0].isdigit() \
+           and q.split(":")[1].split("-")[1].isdigit():
             #get original seq start end end
             qcontig     = q.split(":")[0]
             qsorg,qeorg = q.split(":")[1].split("-")
             qsorg,qeorg = int(qsorg),int(qeorg)
             #get overlapping variants
             if qcontig in chr2variants:
-                vof = filter( lambda x: qsorg <= x[0] < qeorg,chr2variants[qcontig] )
+                vof = filter(lambda x: qsorg <= x[0] < qeorg,chr2variants[qcontig])
         else:
             #define start pos
             qsorg = 0
             if q in chr2variants:
                 vof = chr2variants[q]
         #introduce variants into seq    
-        seq,snps,indels = variants2sequence( r.id,r.seq,vof,qsorg,verbose )
+        seq, snps, indels = variants2sequence(r.id, r.seq, vof, qsorg, verbose)
         #report big difference in seq length after snp/indel correction
         if abs(len(seq)-len(r.seq)) > 5:
-            sys.stderr.write( " Warning: Gene %s: %s difference between orginal (%s) and corrected (%s) sequence length!\n" % (r.id,abs(len(seq)-len(r.seq)),len(seq),len(r.seq)) )
+            info = " Warning: Gene %s: %s difference between orginal (%s) and corrected (%s) sequence length!\n"
+            sys.stderr.write(info % (r.id, abs(len(seq)-len(r.seq)), len(seq), \
+                                     len(r.seq)))
         #report entry with variants
-        out.write( ">%s snps:%s indels:%s\n%s\n" % ( r.id,snps,indels,"\n".join(seq[i:i+60] for i in range(0, len(seq), 60))))
+        line = ">%s snps:%s indels:%s\n%s\n"
+        out.write(line % (r.id, snps, indels, \
+                          "\n".join(seq[i:i+60] for i in range(0, len(seq), 60))))
         
 def main():
 
     usage  = "usage: %(prog)s [options]"
-    parser  = argparse.ArgumentParser( usage=usage,description=desc,epilog=epilog )
+    parser  = argparse.ArgumentParser(usage=usage, description=desc, epilog=epilog)
     
     parser.add_argument("-v", dest="verbose", default=False, action="store_true", help="verbose")    
     parser.add_argument('--version', action='version', version='1.0')
@@ -108,27 +120,31 @@ def main():
                         help="vcf file with SNPs  [stdin]")
     parser.add_argument("--noindels",        default=False, action="store_true", 
                         help="skips INDELs")    
+    parser.add_argument("--nohetero",        default=False, action="store_true", 
+                        help="skip heterozygous SNPs")    
     parser.add_argument("-g", dest="genome",  type=file, 
                         help="genome fasta file   [mandatory]")
     parser.add_argument("-o", dest="out",     default=sys.stdout, type=argparse.FileType('w'), 
                         help="define output name  [stdout]")
-    parser.add_argument("-1", dest="onebased",default=False, action="store_true", 
-                        help="1-based coordinates [%(default)s]")
+    #parser.add_argument("-1", dest="onebased",default=False, action="store_true", 
+    #                    help="1-based coordinates [%(default)s]")
   
     o = parser.parse_args()
     if o.verbose:
         sys.stderr.write( "Options: %s\n" % str(o) )
 
     #go
-    vcf2fasta( o.out,o.vcf,o.genome,o.onebased,o.noindels,o.verbose )
+    vcf2fasta(o.out, o.vcf, o.genome, o.noindels, o.nohetero, o.verbose)
 
 
 if __name__=='__main__': 
-  t0=datetime.now()
-  main()
-  dt=datetime.now()-t0
-  sys.stderr.write( "#Time elapsed: %s\n" % dt )
+    t0 = datetime.now()
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.stderr.write("\nCtrl-C pressed!      \n")
+    except IOError as e:
+        sys.stderr.write("I/O error({0}): {1}\n".format(e.errno, e.strerror))
+    dt = datetime.now()-t0
+    sys.stderr.write("#Time elapsed: %s\n"%dt)
   
-
-
-
