2c942ff4058b61e9f4648fe807023de6f4583790
lpryszcz <l.p.pryszcz@gmail.com>
2014-07-17 12:36:18 +0200

    taxid2metaphors_proteomes.py with group names

--- taxid2metaphors_proteomes.py
+++ taxid2metaphors_proteomes.py
@@ -7,25 +7,25 @@ Mizerow, 17/07/2014
 
 import argparse, MySQLdb, os, sys
 from datetime import datetime
-#sys.path.insert(0, '/users/tg/lpryszcz/cluster/rapsi/src')
-#from taxonomy import Taxonomy
+sys.path.insert(0, '/users/tg/lpryszcz/cluster/rapsi/src')
+from taxonomy import Taxonomy
 sys.path.insert(0, '/users/tg/lpryszcz/cluster/metaphors/src/client')
 import dbClient
 
-def taxid2proteomes(m, name, verbose): #taxa, group_taxid, verbose):
+def taxid2proteomes(m, taxa, group_taxid, verbose):
     """Fetch proteomes for given taxid"""
     #get group info
-    #parent_id, name, rank = taxa.get_taxa_info(group_taxid)
-    outdir = name #"%s_%s"%(group_taxid, name)
+    parent_id, name, rank = taxa.get_taxa_info(group_taxid)
+    outdir = "%s_%s"%(group_taxid, name) #name
     if not os.path.isdir(outdir):
         os.makedirs(outdir)
     #
-    #taxids = filter(lambda taxid: group_taxid in taxa.taxid2lineage(taxid), m.species)
-    taxids = filter(lambda taxid: name in m.species[taxid][-1], m.species)
+    taxids = filter(lambda taxid: group_taxid in taxa.taxid2lineage(taxid), m.species)
+    #taxids = filter(lambda taxid: name in m.species[taxid][-1], m.species)
     if verbose:
-        #info = "Fetching %s taxa for %s [%s %s]...\n"
-        #sys.stderr.write(info%(len(taxids), group_taxid, name, rank))
-        sys.stderr.write("Fetching %s taxa for %s...\n"%(len(taxids), name))
+        info = "Fetching %s taxa for %s [%s %s]...\n"
+        sys.stderr.write(info%(len(taxids), group_taxid, name, rank))
+        #sys.stderr.write("Fetching %s taxa for %s...\n"%(len(taxids), name))
     #save fasta
     for i, taxid in enumerate(taxids, 1):
         spname = m.species[taxid][1]
@@ -45,7 +45,7 @@ def main():
                         help="verbose")
     parser.add_argument('-d', '--db', default="metaphors_201405",
                         help="database name     [%(default)s]")
-    parser.add_argument('-t', '--taxids', nargs="+", #type=int,
+    parser.add_argument('-t', '--taxids', nargs="+", type=int,
                         help="group taxid(s)    [%(default)s]")
     parser.add_argument("--taxadb",        default="/users/tg/lpryszcz/cluster/rapsi/taxonomy.db3",
                         help="taxonomy path  [%(default)s]")
@@ -55,7 +55,7 @@ def main():
         sys.stderr.write("Options: %s\n"%str(o))
         
     #init taxonomy
-    #taxa = Taxonomy(o.taxadb)
+    taxa = Taxonomy(o.taxadb)
 
     #init metaphors connection
     m  = dbClient.metaphors(o.db)
