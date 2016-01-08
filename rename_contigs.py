f03c7d1ff0c3c61b7b20f3d2b3d4bd643e479898
Leszek Pryszcz <l.p.pryszcz@gmail.com>
2016-01-08 08:54:48 +0100

    rename_fasta

--- rename_contigs.py
+++ rename_contigs.py
@@ -1,44 +1,24 @@
 #!/usr/bin/env python
-###
-#
-###
+# Unify the fasta headers. Solve problems with | and other non-standard characters that cause some programs to fail. 
+# USAGE: cat contigs.fa | rename_contigs.py [scaffold] > renamed.fa
+
 
 import os,sys
 from datetime import datetime
 from Bio import SeqIO
 
-def main():
+def main(basename="cotnig"):
   
   for i, r in enumerate(SeqIO.parse(sys.stdin, 'fasta'), 1):
-  	contig = "contig%5i" % i
+  	contig = "%s%5i" % (basename, i)
   	contig = contig.replace(' ', '0')
   	sys.stdout.write(">%s\n%s\n" % (contig, "\n".join(str(r.seq[j:j+60]) for j in range(0, len(r), 60))))
-  	'''#r.description
-    header = r.id
-    
-    contig="contig%s" % i
-    if not header.startswith( ('supercontig','contig') ): 
-      while contig in contigs: 
-        i+=1
-        contig="contig%s" % i
-      header=contig #"%s lenght:%s" % ( contig,len(r.seq) )
-      contigs.append(contig)
-    else: contigs.append(r.id)
-    
-    j=0
-    fasta=">%s\n%s\n" % ( header,r.seq[j:j+window])
-    while j<=len(r.seq):
-      j+=window
-      fasta+='%s\n' % r.seq[j:j+window]
-      
-    out.write( fasta )
-    
-    n=str(r.seq).count('N')
-    sys.stderr.write( "%s %s Ns (%2.2f%s)\n" % (header,n,n*1.0/len(r.seq),'%') )
-  out.close()'''
 
 if __name__=='__main__': 
   t0=datetime.now()
-  main()
+  basename = "contig"
+  if len(sys.argv)>1:
+    basename = sys.argv[1]
+  main(basename)
   dt=datetime.now()-t0
   sys.stderr.write( "Time elapsed: %s\n" % dt )
