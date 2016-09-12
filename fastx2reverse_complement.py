9ed478ffe716e3439fe53677ca8dfd2128082fa5
lpryszcz <l.p.pryszcz@gmail.com>
2016-09-12 22:21:32 +0200

    FastaIndex.py

---
+++ fastx2reverse_complement.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# Convert sequence to reverse complement. Assumes FastA input / output. 
+
+# USAGE: cat fastq | fastx2reverse_complement.py [fastq]
+
+import sys
+from Bio import SeqIO
+
+out = sys.stdout
+
+seqformat = "fasta"
+if len(sys.argv)>1:
+  seqformat = sys.argv[1]
+
+for r in SeqIO.parse(sys.stdin, seqformat):
+  rc = r.reverse_complement()
+  rc.id, rc.description = r.id, r.description
+  out.write(rc.format(seqformat))
+
