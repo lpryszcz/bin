7308ab9eef39078ee1543b824e268dc252f40957
lpryszcz <l.p.pryszcz@gmail.com>
2016-11-07 16:17:21 +0100

    bed2cnv_plot.py

--- bam2cnv.py
+++ bam2cnv.py
@@ -71,8 +71,8 @@ def bam2array(args):
             _arrays[ii][w] += 1
     return i, _arrays
               
-def get_arrays(sizes, windowSize, chr2window, bam, mapq, threads, verbose=1, flag=3844):
-    """Return contact matrix based on BAM"""
+def get_arrays(sizes, windowSize, chr2window, bam, mapq, threads, flag=3844, verbose=1):
+    """Return coverage in wondows based on alignments from BAM"""
     algs = 0
     arrays = []
     if verbose:
@@ -87,37 +87,68 @@ def get_arrays(sizes, windowSize, chr2window, bam, mapq, threads, verbose=1, fla
         logger("  %s alignments parsed"%algs)
     return arrays
 
-def bam2sv_ref(bam, fasta, out, windowSize, mapq=10, threads=4, verbose=1, ploidy=2, mindiff=0.75):
+def get_consecutive(data, stepsize=1):
+    """Return consecutive windows allowing given max. step size"""
+    return np.split(data, np.where(np.diff(data) > stepsize)[0]+1)
+    
+def get_cnvs(name, diff, windows, chrs, minlen, mindiff, stepsize, verbose):
+    """Return collapsed list of CNVs"""
+    # note, a CNV may span across chromosomes, thus need to process chromosomes separately
+    cnvs = []
+    for s, e in chrs:
+        # need to add s to arg
+        cnvs += filter(lambda x: len(x)>=minlen, get_consecutive(np.argwhere(diff[s:e] > mindiff).T[0]+s, stepsize))
+    # collapse
+    coords = []
+    for i, w in enumerate(cnvs, 1):
+        s, e = w[0], w[-1]
+        (chrom, start, end), (_chrom, _start, _end) = windows[s], windows[e]
+        if chrom != _chrom: print "[ERROR] Wrong chromosome start and end: ", windows[w[0]], windows[w[-1]]
+        mean = np.mean(diff[s:e+1])
+        coords.append((chrom, start, _end, name%i, mean))
+    return coords
+    
+def bam2sv_ref(bam, fasta, outbase, windowSize, mapq=10, threads=4, ploidy=2, 
+               mindiff=0.75, minlen=10, stepsize=3, flag=3844, verbose=1):
     """Report likely duplications & deletions from coverage"""
     # generate windows
     windowSize, windows, chr2window = fasta2windows(fasta, windowSize, verbose)
-    
-    if os.path.isfile("%s.npz"%out):
-        npy = np.load('%s.npz'%out)
+
+    # get array
+    if os.path.isfile("%s.npz"%outbase):
+        npy = np.load('%s.npz'%outbase)
         arrays = npy[npy.files[0]]
     else:
         sizes = [len(w) for w in windows]
-        arrays = get_arrays(sizes, windowSize, chr2window, bam, mapq, threads, verbose)
+        arrays = get_arrays(sizes, windowSize, chr2window, bam, mapq, threads, flag, verbose)
         # save
-        np.savez_compressed(out, arrays)
-    
-    # filter windows with gaps
+        np.savez_compressed(outbase, arrays)
     
     # report
+    logger("Reporting CNVs...")
     for ii, _windowSize in enumerate(windowSize):
+        outfn = "%s.%s.bed"%(outbase, _windowSize)
+        print "## %s bp window to %s\n# fname\t# DUPs\t# DELs"%(_windowSize, outfn)
+        # get chr boundaries
+        starts = sorted(chr2window[ii].values())+[len(windows[ii])]
+        chrs = [(starts[i], starts[i+1]) for i in range(len(starts)-1)]
+        #
+        cnvs = []
         # get normalised ref coverage
-        ref = ploidy*arrays[0][ii]/arrays[0][ii].mean()
+        ref = ploidy * arrays[0][ii] / arrays[0][ii].mean()
         for jj, bamfn in enumerate(bam[1:], 1):
-            s = 2*arrays[jj][ii]/arrays[jj][ii].mean()
-            # putative dup
-            dups = filter(lambda x: len(x)>=5, consecutive(np.argwhere(s-ref > mindiff).T[0], stepsize=3))
-            # putative del
-            dels = filter(lambda x: len(x)>=5, consecutive(np.argwhere(ref-s > mindiff).T[0], stepsize=3))
-            print bamfn, len(dups), len(dels)
+            s = ploidy * arrays[jj][ii] / arrays[jj][ii].mean()
+            # putative dups and dels
+            dups = get_cnvs("DUP%s_"+bamfn, s-ref, windows[ii], chrs, minlen, mindiff, stepsize, verbose)
+            dels = get_cnvs("DEL%s_"+bamfn, ref-s, windows[ii], chrs, minlen, mindiff, stepsize, verbose) 
+            print "%s\t%s\t%s"%(bamfn, len(dups), len(dels))
+            cnvs += dels + dups#print dels; print dups
+        #report
+        with open(outfn, "w") as out:
+            out.write("#chrom\tstart\tend\tname\tploidy change\n")
+            for chrom, s, e, name, mean in sorted(cnvs):
+                out.write("%s\t%s\t%s\t%s\t%.3f\n"%(chrom, s, e, name, mean))
 
-def consecutive(data, stepsize=1):
-    return np.split(data, np.where(np.diff(data) > stepsize)[0]+1)
-        
 def main():
     import argparse
     usage   = "%(prog)s -v" #usage=usage, 
@@ -132,23 +163,30 @@ def main():
                         help="Genome FastA file")
     parser.add_argument("-o", "--out", default='cnv',
                         help="output name [%(default)s]")
+    parser.add_argument("-p", "--ploidy", default=2, type=int,
+                        help="ploidy to report CNVs [%(default)s]")
     parser.add_argument("-q", "--mapq", default=10, type=int, 
                         help="min mapping quality for variants [%(default)s]")
     parser.add_argument("-w", "--windowSize", nargs="+", default=[100], type=int,
                         help="window size [%(default)s]")
     parser.add_argument("-t", "--threads", default=4, type=int,
                         help="no. of processors to use [%(default)s]")
-    '''parser.add_argument("-r", "--regions", nargs="+", default=[], 
-                        help="work only in these genomic regions [all]")
-    parser.add_argument("-x", "--bed", default='', 
-                        help="BED file with intervals to exclude")'''
+    parser.add_argument("-d", "--mindiff", default=0.75, type=float,
+                        help="min difference in coverage to call CNV in given window [%(default)s]")
+    parser.add_argument("-l", "--minlen", default=10, type=int,
+                        help="min. no. of windows to call CNV [%(default)s]")
+    parser.add_argument("-s", "--stepsize", default=3, type=int,
+                        help="max distance between consecutive windows within CNV [%(default)s]")
+    parser.add_argument("-F", "--flag", default=3844, type=int,
+                        help="bitflag of alignments to filter out [%(default)s]")
 
     o = parser.parse_args()
     if o.verbose:
         sys.stderr.write("Options: %s\n"%str(o))
         
     # process
-    bam2sv_ref(o.bam, o.fasta, o.out, o.windowSize, o.mapq, o.threads, o.verbose)
+    bam2sv_ref(o.bam, o.fasta, o.out, o.windowSize, o.mapq, o.threads, o.ploidy, 
+               o.mindiff, o.minlen, o.stepsize, o.flag, o.verbose)
 
 if __name__=='__main__': 
     t0 = datetime.now()
---
+++ bed2cnv_plot.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# USAGE: ./bed2cnv_plot.py cnv.100.bed Title
+
+
+import sys
+# http://matplotlib.org/examples/api/two_scales.html
+import numpy as np
+import matplotlib.pyplot as plt
+
+def load_bed(fnames, minSize=0):
+  """Return chrs and sizes of cnvs within each chromosome"""
+  chrom2cnvs = {}
+  for fn in fnames:
+    for l in open(fn):
+      if l.startswith('#'): continue
+      chrom, s, e = l[:-1].split()[:3]
+      s, e = int(s), int(e)
+      size = e-s
+      if size<minSize: continue
+      if chrom not in chrom2cnvs:
+        chrom2cnvs[chrom] = []
+      chrom2cnvs[chrom].append(size)
+  # get sorted chromosomes and cnv sizes
+  chrs = sorted(filter(lambda x: x.isdigit(), chrom2cnvs), key=lambda x: int(x)) # .startswith('DS')
+  cnvs = [chrom2cnvs[chrom] for chrom in chrs]
+  # add the rest
+  chrs.append("*")
+  cnvs.append([])
+  for chrom in filter(lambda x: x not in chrs, chrom2cnvs):
+    cnvs[-1]+= chrom2cnvs[chrom]
+  return chrs, cnvs
+
+fnames = sys.argv[1:-1]
+title = sys.argv[-1]
+
+chrs, cnvs = load_bed(fnames)
+
+width, spacer = 0.4, 0.1
+color1, color2 = 'b', 'r'
+
+ind = np.arange(len(chrs))#; print chrs, ind
+
+fig, ax1 = plt.subplots()
+ax1.bar(ind+spacer, map(len, cnvs), width, color=color1)
+ax1.set_xlabel('chromosome')
+# Make the y-axis label and tick labels match the line color.
+ax1.set_ylabel('number of CNVs', color=color1)
+for tl in ax1.get_yticklabels(): tl.set_color(color1)
+
+ax2 = ax1.twinx()
+ax2.bar(ind+width+spacer, np.array(map(sum, cnvs))/1e6, width, color=color2)
+ax2.set_ylabel('cummulative size of CNVs [Mb]', color=color2)
+for tl in ax2.get_yticklabels(): tl.set_color(color2)
+
+ax1.set_xticks(ind+width+spacer)
+ax1.set_xticklabels(chrs)
+
+ax1.set_title(title)
+
+ext = ".png"
+if len(fnames)==1:
+  fname = fnames[0]+ext
+else:
+  fname = "all%s"%ext
+print "Saving figure as %s"%fname
+plt.savefig(fname)
+
+
