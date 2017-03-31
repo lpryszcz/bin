23e479141f63ba0496bc139a1a4a3ddc8a8a32f3
Leszek <l.p.pryszcz@gmail.com>
2017-03-31 09:42:38 +0200

    exonerate2ptc.py

--- exonerate2ptc.py
+++ exonerate2ptc.py
@@ -11,6 +11,7 @@ Carmona, 29/03/2017
 
 import os, sys
 from datetime import datetime
+from FastaIndex import FastaIndex
 
 def _get_alignement(handle):
     """Return alignement"""
@@ -19,22 +20,24 @@ def parse_exonerate(handle):
     """parse exonerate output"""
     alg = ['', '', '', '']
     ii = 0
-    for l in handle:
-        l = l[:-1]
+    while True:
+        l = handle.readline()
+        if not l: break
+        l = l[:-1]#; print l
         # skip header and spacer lines
-        if l.startswith(('Command line:', 'Hostname:', '------------')) or not l:
+        if l.startswith(('Command line:', 'Hostname:', '--')) or not l:
             continue
         # alg start - skip header
         if l.startswith('C4 Alignment:'):
-            while l!='\n':
-                l = handle.readline()
+            while l:
+                l = handle.readline()[:-1]
         # read vulgar and report
         elif l.startswith('vulgar: '):
             ii = 0
-            ldata = l.split(': ')[-1]
-            t, ts, te, tstrand, q, qs, qe, qstrand = ldata[:8]
+            ldata = l.split(': ')[-1].split()
+            q, qs, qe, qstrand, t, ts, te, tstrand = ldata[:8]#; print ldata
             ts, te, qs, qe = map(int, (ts, te, qs, qe))
-            yield t, ts, te, tstrand, q, qs, qe, qstrand, alg
+            yield q, qs, qe, qstrand, t, ts, te, tstrand, alg
         # read alignment
         else:
             if ii%4 in (0, 3):
@@ -43,10 +46,21 @@ def parse_exonerate(handle):
             else:
                 l = l[-len(pl):]
             alg[ii%4] += l
+            ii += 1    
             
 
-def exonerate2ptc(handle, out, verbose):
+def exonerate2ptc(handle, out, pepfn, minoverlap, verbose):
     """Return premature stop codons and frame-shifts from exonerate output"""
+    faidx = FastaIndex(pepfn)
+    pep2len = {pep: data[0] for pep, data in faidx.id2stats.iteritems()}
+    for q, qs, qe, qstrand, t, ts, te, tstrand, alg in parse_exonerate(handle):
+        alglen =  map(len, alg)
+        if len(set(alglen))>1:
+            print "error", q, t, alglen
+        if (qe-qs)/pep2len[q] < minoverlap:
+            print t, q, alglen#; print '\n'.join(alg); return
+            continue
+        
 
 def main():
     import argparse
@@ -59,6 +73,8 @@ def main():
     parser.add_argument("-i", "--input", default=sys.stdin, type=file, help="input stream [stdin]")
     parser.add_argument("-o", "--output", default=sys.stdout, type=argparse.FileType('w'), 
                         help="output stream [stdout]")
+    parser.add_argument("-p", "--pep", required=1, help="peptides FastA file")
+    parser.add_argument("--overlap", default=0.9, help="min. fraction of query aligned [%(default)s]")
 
     '''# print help if no parameters
     if len(sys.argv)==1:
@@ -68,6 +84,7 @@ def main():
     if o.verbose:
         sys.stderr.write("Options: %s\n"%str(o))
 
+    exonerate2ptc(o.input, o.output, o.pep, o.overlap, o.verbose)
 	
 if __name__=='__main__': 
     t0 = datetime.now()
