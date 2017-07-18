457eee60b0c3d13175b62814f16698dd31bf4ff8
Leszek Pryszcz <lpryszcz@iimcb.gov.pl>
2017-07-18 12:01:03 +0200

    grep.py with -s

--- grep.py
+++ grep.py
@@ -1,25 +1,38 @@
 #!/usr/bin/env python
-### Fast `grep -f` implementation for GTF files
-# USAGE: grep.py file_with_elements file_to_grep
+### Fast `grep -f` implementation for GTF files. If -s, then IDs are split by `.`.
+# USAGE: grep.py [-s] file_with_elements file_to_grep
 
 import sys
 
-if len(sys.argv)<3:
-  sys.exit("Provide two arguments!")
+params = filter(lambda x: x.startswith("-"), sys.argv[1:])
+fnames = filter(lambda x: not x.startswith("-"), sys.argv[1:])
 
-fn1, fn2 = sys.argv[1:3]
+split = False
+if "-s" in params:
+  split = True
+  
+if len(fnames)!=2:
+  sys.exit("Provide two files!")
+
+fn1, fn2 = fnames[:2]
 
 if fn2 == "-":
   handle2 = sys.stdin
 else:
   handle2 = open(fn2)
-  
-lines = set(l[:-1] for l in open(fn1))
+
+def dotsplit(s, split=True):
+  """Split string by dot and return first element"""
+  if split:
+    return s.split('.')[0]
+  return s
+
+lines = set(dotsplit(l[:-1] ,split) for l in open(fn1))
 
 for i, l in enumerate(handle2, 1):
   if not i%100:
     sys.stderr.write(" %i      \r"%i)
-  if lines.intersection([e.strip(';').strip('"') for e in l.split(' ',)]):
+  if lines.intersection([dotsplit(e.strip(';').strip('"'), split) for e in l.split(' ',)]):
     sys.stdout.write(l)
     
 
