b9ac09a761b647c9afb90bcb2aa739fc3ba2c24a
Leszek Pryszcz <lpryszcz@iimcb.gov.pl>
2017-07-18 11:41:48 +0200

    grep.py peyote2

--- tab2rounded.py
+++ tab2rounded.py
@@ -14,13 +14,16 @@ for i, l in enumerate(sys.stdin):
     if not i:
         sys.stdout.write(l)
         continue
-    ldata = l[:-1].split()
-    gid = ldata[0]
+    ldata = l[:-1].split('\t')
+    gid = ldata[0].replace(' ', '_')
     if gid in ids:
+        sys.stderr.write('[WARNING] Duplicated ID: %s\n'%gid)
+        #'''
         if gid.split('.')[-1].isdigit():
-            gid = ".".join(gid.split('.')[:-1])+int(gid.split('.')[-1])+1
+            gid = ".".join(gid.split('.')[:-1])+str(int(gid.split('.')[-1])+1)
         else:
             gid = gid+".1"
+        #'''
     ids.add(gid)
     ndata = [gid,] + [str(int(round(float(x)*norm))) for x in ldata[1:]]
     sys.stdout.write("\t".join(ndata)+"\n")    
