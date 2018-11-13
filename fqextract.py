f3ccb48b442f876815f2bd5e54dd27111cb6c95e
Leszek <lpryszcz@solaris>
2018-11-13 14:40:56 +0100

    fqextract.py

--- fqextract.py
+++ fqextract.py
@@ -9,16 +9,17 @@ if rnameFn.endswith(".gz"):
 else:
     rnameFile = open(rnameFn)
 
+out = sys.stdout
 ids, flag = set('@' + x.split()[0] for x in rnameFile), False
 sys.stderr.write(" %s read names loaded.\n"%len(ids))
 k=0
 for lineno, l in enumerate(sys.stdin, 1):
-    if lineno%1e5:
-        sys.stderr.write(" %i\r"%lineno)
+    if not lineno%1e5:
+        sys.stderr.write(" %i processed with %i matche(s)\r"%(lineno/4, k/4))
     if lineno%4 == 1:
         flag = (l.split()[0] in ids)
     if flag:
-        print l,
+        out.write(l)
         k+=1
     
 sys.stderr.write("%s extracted from %s reads\n"%(k/4, lineno/4))
