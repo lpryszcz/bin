3e9d05351e2902e4b6b5d0beaf6adc546b13f4c7
Leszek <lpryszcz@crg.es>
2019-06-26 14:59:07 +0200

    samtools2stats.sh

--- fastx2reverse.py
+++ fastx2reverse.py
@@ -6,18 +6,31 @@
 import sys
 #from Bio import SeqIO
 
-out = sys.stdout
-
 seqformat = "fasta"
 if len(sys.argv)>1:
   seqformat = sys.argv[1]
-if seqformat != "fasta":
-  sys.exit("No FastQ support")
 
-for l in sys.stdin:
-  if l.startswith(">"):
-    out.write(l)
-  else:
-    out.write("".join(reversed(l[:-1]))+"\n")
+def process_fasta(handle, out=sys.stdout):
+  """Reverse seq in FastA format"""
+  for l in sys.stdin:
+    if l.startswith(">"):
+      out.write(l)
+    else:
+      out.write("".join(reversed(l[:-1]))+"\n")
+      
+def process_fastq(handle, out=sys.stdout):
+  """Reverse seq in FastQ format assuming 4-lines per entry"""
+  for i, l in enumerate(sys.stdin):
+    # seq and qual are odd lines
+    if i%2:
+      out.write("".join(reversed(l[:-1]))+"\n")
+    # header and spacer (+) are even lines
+    else:
+      out.write(l)
 
-
+if seqformat=="fasta":
+  process_fasta(sys.stdin)
+elif seqformat=="fastq":
+  process_fastq(sys.stdin)
+else:
+  sys.exit("Sequence format (%s) not recognised!"%seqformat)
