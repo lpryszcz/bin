baa88a53130d68b7cc95e91017d5adccadac9eb8
Leszek <lpryszcz@crg.es>
2019-06-26 15:28:56 +0200

    bam2stats.sh

--- samtools2stats.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Report stats from samtools stats for bam file ignoring secondary, suplementary and qc failed alignments
-
-samtools stats -F3840 $1 | awk '{if ($2=="sequences:"){nseq=$3} else if($2=="reads" && $3=="mapped:"){printf("mapped: %s (%.1f%)\n",$4,100*$4/nseq)} else if($2=="error" && $3=="rate:"){printf("identity: %.2f%\n",100-$4*1e2)}}'
-
-
