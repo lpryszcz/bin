a19400c05263fffa319eafaa1d31ce9464d8332b
Leszek <lpryszcz@crg.es>
2019-06-26 15:34:10 +0200

    bam2stats.sh

--- bam2stats.sh
+++ bam2stats.sh
@@ -4,7 +4,7 @@
 #
 # USAGE: bam2stats.py bam1 bam2 ... bamN
 
-for f in "$@"; do echo $f `samtools stats -F3840 $1 | awk '{if ($2=="sequences:"){nseq=$3} else if($2=="reads" && $3=="mapped:"){printf("mapped: %s (%.1f%)\n",$4,100*$4/nseq)} else if($2=="error" && $3=="rate:"){printf("identity: %.2f%\n",100-$4*1e2)}}'`
+for f in "$@"; do echo $f `samtools stats -F3840 $f | awk '{if ($2=="sequences:"){nseq=$3} else if($2=="reads" && $3=="mapped:"){printf("mapped: %s (%.1f%)\n",$4,100*$4/nseq)} else if($2=="error" && $3=="rate:"){printf("identity: %.2f%\n",100-$4*1e2)}}'`
 done
 
 
