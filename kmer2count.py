2c9048e1b0013a44f2909acd35c7580e21e61e92
Leszek <lpryszcz@crg.es>
2020-09-28 15:56:42 +0200

    dropbox-crg

--- kmer2count.py
+++ kmer2count.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # Report counts of mers from FastA file
-#USAGE: mer2count.py FastA [5 10]
+# USAGE: mer2count.py FastA [5 10]
 
 import os, sys, pysam
 #from Bio import SeqIO
@@ -10,10 +10,11 @@ fastafn = sys.argv[1]
 if len(sys.argv)>3:
     n, m = map(int, sys.argv[-2:])
 
+bases = set(list("ACGT"))
 faidx = pysam.FastaFile(fastafn)
 for k in range(n, m+1):
     mers = set()
     for ref in faidx.references:
         seq = faidx[ref]
-        mers.update(set(seq[s:s+k] for s in range(len(seq)-k)))
+        mers.update(set(seq[s:s+k] for s in range(len(seq)-k) if len(bases.union(seq[s:s+k]))==4))
     print(k, len(mers), 4**k, len(mers)/4**k)
