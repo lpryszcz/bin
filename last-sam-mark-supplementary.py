6f95e7a0361f5a050980846c77e13c4948318654
Leszek <lpryszcz@crg.es>
2021-06-09 12:02:40 +0200

    romanChr2int.py

--- last-sam-mark-supplementary.py
+++ last-sam-mark-supplementary.py
@@ -3,8 +3,10 @@
 # this works for maf-convert sam | samtools view -ShT $ref algs
 import os, sys, pysam
 
-def store_algs(out, read_algs):
-    """Store only reads with mapq lower than max as supplementary algs"""
+def store_algs0(out, read_algs):
+    """Mark alignments with score (AS:i) lower than max score for given read
+    as supplementary algs
+    """
     if len(read_algs)>1:
         mapq = [a.mapq for a in read_algs]
         maxq = max(mapq)
@@ -16,6 +18,26 @@ def store_algs(out, read_algs):
     # report algs
     for a in read_algs: out.write(a)
 
+def get_alignment_score(a):
+    """Return alignment score"""
+    k2v = {k: v for k, v in a.tags}
+    return k2v["AS"]
+
+def store_algs(out, read_algs):
+    """Mark alignments with score (AS:i) lower than max score for given read
+    as supplementary algs
+    """
+    if len(read_algs)>1:
+        alg_scores = [get_alignment_score(a) for a in read_algs]
+        max_score = max(alg_scores)
+        # skip cases where more than 1 alg has top mapq
+        if len([s for s in alg_scores if s==max_score])>1: max_score+=1
+        # mark supplementary
+        for a, s in zip(read_algs, alg_scores):
+            if s<max_score: a.is_supplementary = True
+    # report algs
+    for a in read_algs: out.write(a)
+    
 #samfn = "/home/lpryszcz/cluster/rna_mods/tRNA/_archives/raw/RNA222222.hac.lastal.T0Q1trainD100.maf.sam"
 sam = pysam.AlignmentFile(sys.stdin) 
 out = pysam.AlignmentFile(sys.stdout,  "wb", template=sam)
---
+++ romanChr2int.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# Convert arabic chr (IV) into int (4).
+# Can handle chr. 
+
+import sys
+from functools import reduce
+
+numerals = {'M' : 1000, 'D' : 500, 'C' : 100, 'L' : 50, 'X' : 10, 'V' : 5, 'I' : 1}
+def romannumeral2number(s):
+    return reduce(lambda x, y: -x + y if x < y else x + y, map(lambda x: numerals.get(x, 0), s.upper()))
+
+handle = sys.stdin
+out = sys.stdout
+for l in handle:
+    #if l.startswith(">"):
+    ref = l.split()[0]
+    addchr = ""
+    if ref.lower().startswith('chr'):
+        addchr = ref[:3]
+        ref = ref[3:]
+    nref = romannumeral2number(ref)
+    if addchr: ref = addchr+ref
+    l = "%s"%nref + l[len(ref):]
+    out.write(l)
