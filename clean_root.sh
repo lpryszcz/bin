6759194877e0a6ce12b177cf9c2f632775103a5e
Leszek <lpryszcz@crg.es>
2021-12-22 08:00:52 +0100

    clean

--- clean_root.sh
+++ clean_root.sh
@@ -5,6 +5,9 @@
 # clean /var/apt
 sudo apt-get clean
 
+echo "20 largest dpkg packeges:"
+dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -n | tail -n20
+
 # retain max 2 version of each snap - this can't be lower than 2
 sudo snap set system refresh.retain=2
 # clean /var/snapd
