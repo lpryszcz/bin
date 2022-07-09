47be3054472d85b7ac42d21d55c59ca44eb8c2e2
Leszek <l.p.pryszcz@gmail.com>
2022-07-09 21:47:42 +0200

    update_spotify.sh

---
+++ update_spotify.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+cd ~/Music/spotify
+# download daily 1-6, discover weekly, radar & liked
+spotify_dl -s -w -l \
+	   https://open.spotify.com/playlist/37i9dQZF1E3783ZEsHDeht \
+	   https://open.spotify.com/playlist/37i9dQZF1E38RsxzxUTZRk \
+	   https://open.spotify.com/playlist/37i9dQZF1E38cTYLyGroCC \
+	   https://open.spotify.com/playlist/37i9dQZF1E36VpRu7l05xK \
+	   https://open.spotify.com/playlist/37i9dQZF1E390FOdL3Y9Gq \
+	   https://open.spotify.com/playlist/37i9dQZF1E38dIUgR6Sw1c \
+	   https://open.spotify.com/playlist/37i9dQZEVXcJgVGv6YnNez \
+	   https://open.spotify.com/playlist/37i9dQZEVXbv5k5KXCSJOI
+#	   https://open.spotify.com/playlist/1LNJ6u1kpUlwV2Ibx23wbo?si=afd1b0565b904285 
+
