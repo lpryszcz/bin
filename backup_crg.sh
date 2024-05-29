c7b331de33829b57340fb913be5383f8853a6856
Leszek <lpryszcz@crg.es>
2024-05-29 13:10:14 +0200

    backup_crg both fast5 and pod5

--- backup_crg.sh
+++ backup_crg.sh
@@ -4,12 +4,15 @@
 mkdir -p _toRemove
 mv /ssd/data/ont/*.fast5 _toRemove
 # sync everything once per day @ 6am
-if [ `date +%H` -eq 6 ]; then    
-    rsync -av /ssd/data/ont/ /users/enovoa/data/ont --exclude _toRemove --exclude "*.fast5.bam" --exclude "*.tar.gz" \
+if [ `date +%H` -eq 6 ]; then
+    for d in /ssd/data/ont/ /ssd_backup/data/ont/; do
+	echo `date` $d
+	rsync -av $d /users/enovoa/data/ont --exclude _toRemove --exclude "*.fast5.bam" --exclude "*.tar.gz" \
 	  --exclude "*.tmp" --exclude core-dump-db --exclude intermediate --exclude queued_reads \
 	  --exclude persistence --exclude pings --exclude reads --exclude user_scripts
+    done
 # sync recent Fast5 files every hour
 else
-    find /ssd/data/ont/./ -mmin -61 -type f -name "*.fast5" | grep -v _toRemove > /tmp/rsync_crg.txt
+    find /ssd/data/ont/./ -mmin -61 -type f -name "*.fast5" -o -name "*.pod5" | grep -v _toRemove > /tmp/rsync_crg.txt
     rsync -av --files-from=/tmp/rsync_crg.txt / /users/enovoa/data/ont
 fi
--- remove_old_runs.sh
+++ remove_old_runs.sh
@@ -2,15 +2,18 @@
 # Remove runs older than 91 days that are not in use by any app (no *.toml file in the project directory).
 
 # remove old runs
-cd /ssd/data/ont
-# get all run dirs older than 3 months
-find  -mindepth 2 -maxdepth 2 -type d -mtime +91|grep -vP "persistence|pings|reads|user_scripts|queued_reads|intermediate|core-dump-db|_toRemove" > runs.txt
-# sync and remove those that are not used by the app
-while read d; do
-    p=$(dirname $d);
-    if [ $(find $p -name "*.toml"|wc -l) -eq 0 ]; then
-	du -sh $d;
-	rsync -a $d /users/enovoa/data/ont/$p;
-	rm -r $d;
-    fi;
-done < runs.txt
+for d in /ssd/data/ont/ /ssd_backup/data/ont/; do
+    echo `date` $d
+    cd $d
+    # get all run dirs older than 3 months
+    find  -mindepth 2 -maxdepth 2 -type d -mtime +91|grep -vP "persistence|pings|reads|user_scripts|queued_reads|intermediate|core-dump-db|.nv|_toRemove" > runs.txt
+    # sync and remove those that are not used by the app
+    while read d; do
+	p=$(dirname $d);
+	if [ $(find $p -name "*.toml"|wc -l) -eq 0 ]; then
+	    du -sh $d;
+	    rsync -a $d /users/enovoa/data/ont/$p;
+	    rm -r $d;
+	fi;
+    done < runs.txt
+done
