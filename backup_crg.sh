4967d0c475b7394430b06ffd8bf91301199c31e7
Leszek <lpryszcz@crg.es>
2024-10-14 13:53:18 +0200

    backup_crg.sh

--- backup_crg.sh
+++ backup_crg.sh
@@ -2,18 +2,7 @@
 
 # move all completed bulk dumps to _toRemove
 cd /ssd/data/ont
-mkdir -p _toRemove
-mv *.fast5 _toRemove
-# sync everything once per day @ 6am
-if [ `date +%H` -eq 6 ]; then
-    for d in /ssd/data/ont/ ; do
-	echo `date` $d
-	rsync -av $d /users/enovoa/data/ont --exclude _toRemove --exclude "*.fast5.bam" --exclude "*.tar.gz" \
-	  --exclude "*.tmp" --exclude core-dump-db --exclude intermediate --exclude queued_reads \
-	  --exclude persistence --exclude pings --exclude reads --exclude user_scripts
-    done
-# sync recent pod5 files every hour
-else
-    find -L /ssd/data/ont/./ -mmin -61 -type f -name "*.pod5" | grep -v _toRemove > /tmp/rsync_crg.txt
-    rsync -av --files-from=/tmp/rsync_crg.txt / /users/enovoa/data/ont
-fi
+mkdir -p _toRemove; mv *.fast5 _toRemove
+# consider -ctime 0 # files created in the last day (24h)
+find -L . -type f -cmin -61 -regextype posix-extended -regex ".*.(pod5|html|csv|txt|md|json)" | grep -vwP "_toRemove|reads" > /tmp/rsync_crg.txt
+rsync -av --files-from=/tmp/rsync_crg.txt `pwd` /users/enovoa/data/ont
