6fce0a053fae646b5aac7baef2619e763fe9529a
Leszek <lpryszcz@crg.es>
2024-10-14 14:24:06 +0200

    backup_crg.sh

--- backup_crg.sh
+++ backup_crg.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+echo `date` Starting sync...
 # move all completed bulk dumps to _toRemove
 cd /ssd/data/ont
 mkdir -p _toRemove; mv *.fast5 _toRemove
-# consider -ctime 0 # files created in the last day (24h)
+
+# sync files modified in last 61 minutes
 find -L . -type f -cmin -61 -regextype posix-extended -regex ".*.(pod5|html|csv|txt|md|json)" | grep -vwP "_toRemove|reads" > /tmp/rsync_crg.txt
-rsync -av --files-from=/tmp/rsync_crg.txt `pwd` /users/enovoa/data/ont
+if [ -s /tmp/rsync_crg.txt ]; then
+    echo `date` Syncing `cat /tmp/rsync_crg.txt|wc -l` files...
+    rsync -av --files-from=/tmp/rsync_crg.txt `pwd` /users/enovoa/data/ont
+fi
+echo `date` Finished.
