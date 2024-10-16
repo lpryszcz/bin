23434d4e4dde5260d94220cd4e2087bf5b950644
Leszek <lpryszcz@crg.es>
2024-10-16 05:29:14 +0200

    backup_crg.sh

--- backup_crg.sh
+++ backup_crg.sh
@@ -9,6 +9,6 @@ mkdir -p _toRemove; mv *.fast5 _toRemove
 find -L . -type f -cmin -61 -regextype posix-extended -regex ".*.(pod5|html|csv|txt|md|json)" | grep -vwP "_toRemove|reads" > /tmp/rsync_crg.txt
 if [ -s /tmp/rsync_crg.txt ]; then
     echo `date` Syncing `cat /tmp/rsync_crg.txt|wc -l` files...
-    rsync -av --files-from=/tmp/rsync_crg.txt `pwd` /users/enovoa/data/ont
+    rsync -av --files-from=/tmp/rsync_crg.txt `pwd` /no_backup/enovoa/data/ont
 fi
 echo `date` Finished.
